24500d8604883de6f042df33b83045cf31883781
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-16 00:47:30 +0000

    rando generator working!

--- express_server.js
+++ express_server.js
@@ -40,20 +40,20 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.send(`${generateRandomString()}`);         // Respond with 'Ok' (we will replace this)
 });
 
 const generateRandomString = function(length = 6) {
-  return Math.random().toString(20).substr(2, length)
+return Math.random().toString(20).substr(2, length)
 }
-randoStr = generateRandomString
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
