845b62af4fe8e8438713d6509ef5dbb25370b027
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-16 00:32:24 +0000

    I think these are working?

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -13,13 +15,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// app.get('/urls/:shortURL', (req, res) => {
-//   res.send(req.params.shortURL)
-// })
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
-  res.render("urls_show", templateVars);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+  console.log("User going to New")
 });
 
 app.get("/hello", (req, res) => {
@@ -39,6 +38,22 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const generateRandomString = function(length = 6) {
+  return Math.random().toString(20).substr(2, length)
+}
+randoStr = generateRandomString
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
