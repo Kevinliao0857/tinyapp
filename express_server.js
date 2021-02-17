bfe4b69a398a3d1066f2cdfc72afcae942a86726
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-17 03:57:07 +0000

    login logout working

--- express_server.js
+++ express_server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -42,15 +44,9 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls", (req, res) => {
   console.log("User in URLs")
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
-
-
-// app.get("/u/:shortURL", (req, res) => {
-//   const longURL = urlDatabase[req.params.shortURL]
-//   res.redirect(longURL);
-// });
 
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -66,6 +62,31 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 
+app.post("/login", (req, res) => {
+  const userID = req.body;
+  console.log(userID)
+  res.cookie("username", userID.username);
+  res.redirect("/urls");
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
+
+
+
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { username: req.cookies.username, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
 
 
 app.listen(PORT, () => {
