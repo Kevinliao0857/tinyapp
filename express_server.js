287f3a844a3262cc8d5df0e4ca92a47f33e061ed
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-17 00:57:53 +0000

    somethings off

--- express_server.js
+++ express_server.js
@@ -20,14 +20,14 @@ app.get("/urls/new", (req, res) => {
   console.log("User going to New")
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
-});
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: 'Hello World!' };
+//   res.render("hello_world", templateVars);
+// });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -50,14 +50,20 @@ app.post("/urls", (req, res) => {
   const longURL = req.body.longURL
   const shortURL = generateRandomString()
   urlDatabase[shortURL] = longURL
-  res.redirect(`/urls/${shortURL}`);       
+  res.redirect(`/urls/`);       
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  // console.log(req.params.shortURL)
   const longURL = urlDatabase[req.params.shortURL]
-  // console.log(longURL)
   res.redirect(longURL);
+});
+
+
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL].longURL = req.body.longURL;
+  // delete urlDatabase[shortURL]
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
